22c31871723d0db6900b5934e8d0356b40cddb8d
Erik Bjur <erik@bjurengineering.com>
2023-07-24 23:02:52 -0700

    Added Microsoft c++ extension debugger

--- App/main.cpp
+++ App/main.cpp
@@ -1,13 +1,14 @@
-#include <iostream>
 #include "MyLibrary.h"
+#include <iostream>
+
 
 int main() {
-    std::cout << "Creating New entity" << std::endl;
+  std::cout << "Creating New entity" << std::endl;
 
-    MyLibrary::Entity* myEntity = new MyLibrary::Entity();
+  MyLibrary::Entity *myEntity = new MyLibrary::Entity();
 
-    myEntity->Name = "My Name";
+  myEntity->Name = "My Name";
 
-    myEntity->PrintName();
-    return 0;
+  myEntity->PrintName();
+  return 0;
 }
